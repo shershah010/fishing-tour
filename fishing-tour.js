3e0a4d1b783e153ac80c89ee445f5b1a3925cd66
Sher Shah <sher.shah010@gmail.com>
2019-07-14 09:17:53 -0700

    updated library and css

--- fishing-tour.js
+++ fishing-tour.js
@@ -1,67 +1,73 @@
 class Tips {
 
-  constructor(index) {
-    this.tips = [];
-    this.index = index;
+  constructor() {
+    this.tips = []; // an array that holds all the tips.
 
     window.onclick = (function(event) {
-      var tipBox = event.target.closest(".tipBox");
-      var circles = event.target.closest(".circleContainer");
-      var tipBoxPresent = document.querySelector(".tipBox");
+      const tipBox = event.target.closest(".ft-tip-box");
+      const circles = event.target.closest(".ft-circle-container");
+      const tipBoxPresent = document.querySelector(".ft-tip-box");
       if (tipBox == null && circles == null && tipBoxPresent != null) {
         tipBoxPresent.remove();
-        this.index += 1;
-        this.start();
+        this.start(this.index + 1);
       }
-    }).bind(this);
+    }).bind(this); // if a tip box is open and the user clicks anywhere outside the tip box, then remove the tip box and start the next tip.
   }
 
   add(tip) {
     this.tips.push(tip);
-  }
+  } // simply pushes the next tip in.
 
-  start() {
+  createCircleHTML() {
+    return `<div class='ft-circle' style='animation-delay: -2s'></div>
+            <div class='ft-circle' style='animation-delay: -1s'></div>
+            <div class='ft-circle' style='animation-delay: 0s'></div>`;
+  } // creates the tip icon.
+
+  createTipBoxHTML(header, info, hasFn) {
+    if (!hasFn) {
+      return "<h1>" + header + "</h1><p>" + info + "</p><button class='ft-small-button'>&#10005;</button>";
+    }
+    return "<h1>" + header + "</h1><p>" + info + "</p><button class='ft-small-button'>&#10005;</button><button class='ft-big-button'>Show Me</button>";
+  } // creates the tip box.
+
+  start(index) {
+    this.index = index; // notes which tip to start on
+
     if (this.index >= this.tips.length) {
       return;
-    }
+    } // if all the tips have been shown, then exit.
 
-    const tip = this.tips[this.index];
+    const tip = this.tips[this.index]; // get the tip properties.
     const target = document.querySelector(tip.selector);
     const circle = document.createElement("container");
 
-    target.style.position = "relative";
+    target.style.position = "relative"; // the tip selector must be made relative so that the tip icon and tip box can be displayed properly.
 
-    circle.classList.add("circleContainer");
+    circle.classList.add("ft-circle-container");
     circle.style.top = tip.y;
     circle.style.left = tip.x;
-    circle.innerHTML = "<div class='circle' style='animation-delay: -2s'></div><div class='circle' style='animation-delay: -1s'></div><div class='circle' style='animation-delay: 0s'></div>";
+    circle.innerHTML = this.createCircleHTML();
     circle.addEventListener("click", (function(e) {
-      circle.remove();
+      circle.remove(); // removes the tip icon.
 
       const tipBox = document.createElement("div");
 
-      let next = (function() {
-        tipBox.remove();
-        this.index += 1;
-        this.start();tipBox.remove();
-      }).bind(this);
+      tipBox.classList.add("ft-tip-box");
+      tipBox.innerHTML = this.createTipBoxHTML(tip.header, tip.info, tip.fn !== undefined);
 
-      let outside = (function(e) {
-        if (e.target != tipBox && e.target != circle) {
-          next();
-        }
-      }).bind(this);
+      tipBox.querySelector(".ft-small-button").addEventListener("click", (function(e) {
+        tipBox.remove(); // removes the tip box
+        this.start(this.index + 1); // recursively calls start until the end of the tip array.
+      }).bind(this)); // clicking the small 'x' should close the tip box and show the next tip.
 
-      tipBox.classList.add("tipBox");
-      tipBox.innerHTML = "<h1>" + tip.header + "</h1><p>" + tip.info + "</p><button class='smallButton'>&#10005;</button><button class='bigButton'>Show Me</button>";
-      tipBox.querySelector(".smallButton").addEventListener("click", function(e) {
-        next();
-      });
-
-      tipBox.querySelector(".bigButton").addEventListener("click", function(e) {
-        if (tip.fn !== undefined) { tip.fn(); }
-        next();
-      });
+      if (tip.fn !== undefined) {
+        tipBox.querySelector(".ft-big-button").addEventListener("click", (function(e) {
+          tip.fn();
+          tipBox.remove();
+          this.start(this.index + 1);
+        }).bind(this));
+      } // if a function is given, then run it.
 
       target.appendChild(tipBox);
     }).bind(this));
@@ -72,61 +78,77 @@ class Tips {
 
 class Tour {
 
-	constructor(options) {
+	constructor() {
 		this.steps = [];
-		this.currentStep = 0;
-		this.body = document.querySelector("body");
 	}
 
-	addStep(obj) {
+	add(obj) {
 		this.steps.push(obj);
-		// add a new step but hold on its inialization
-	}
-	start() {
-		this.initializeStep(this.steps[0]);
-		// initalize the first step and start at the first step
-	}
+	} // add a new step but hold on its inialization
+
+	start(index) {
+    this.currentStep = index;
+
+    if (this.currentStep >= this.steps.length) {
+      return;
+    }
+
+		this.initializeStep(this.steps[this.currentStep]);
+	} // initalize the first step and start at the first step
+
 	initializeStep(obj) {
-		var item = document.querySelector(obj.selector);
-		var info = document.createElement("p");
-		info.innerHTML = obj.info;
-		var pos = obj.position;
-		//obj.
-		this.displayStep(item, info, pos)
-		// initalize the step obj
+		const item = document.querySelector(obj.selector);
+		var pos = obj.position; //obj.
+		this.displayStep(item, obj.info, pos); // initalize the step obj
 	}
+
 	deinitializeStep() {
-		document.querySelector(".tourBox").remove();
+		document.querySelector(".ft-tour-box").remove();
 	}
+
+  createTourBoxHTML(info) {
+    return `<p>` + info + `</p>
+            <div>
+              <button>Back</button>
+              <button>Skip</button>
+              <button>Next</button>
+              <button>Done</button>
+            </div>`;
+  }
+
 	displayStep(element, info, pos){
-		var tourBox = document.createElement("div");
-		var buttonBox = document.createElement("div");
-		var backButton = document.createElement("button");
-		backButton.onclick = (function() { this.back(); }).bind(this);
-		backButton.innerHTML = "Back";
-		var nextButton = document.createElement("button");
-		nextButton.onclick = (function() { this.next(); }).bind(this);
-		nextButton.innerHTML = "Next";
-		var doneButton = document.createElement("button");
-		doneButton.onclick = (function() { this.end(); }).bind(this);
-		doneButton.innerHTML = (this.currentStep == this.steps.length) ? "Done" : "Skip";
+		const tourBox = document.createElement("div");
+    tourBox.innerHTML = this.createTourBoxHTML(info);
+    const buttons = tourBox.querySelectorAll("button");
+    buttons[0].addEventListener("click", (function(e) {
+      this.back();
+    }).bind(this)); // Back button
+    buttons[1].addEventListener("click", (function(e) {
+      this.end();
+    }).bind(this)); // Skip button
+    buttons[2].addEventListener("click", (function(e) {
+      this.next();
+    }).bind(this)); // Next button
+    buttons[3].addEventListener("click", (function(e) {
+      this.end();
+    }).bind(this)); // Done button
+
 		if (this.currentStep == 0) {
-			buttonBox.appendChild(doneButton);
-			buttonBox.appendChild(nextButton);
-		} else if (count == steps.length) {
-			buttonBox.appendChild(backButton);
-			buttonBox.appendChild(doneButton);
-		} else {
-			buttonBox.appendChild(doneButton);
-			buttonBox.appendChild(backButton);
-			buttonBox.appendChild(nextButton);
+			buttons[0].classList.add("ft-hidden");
 		}
-    var placement = this.findPlacement(element, pos)
-		tourBox.appendChild(info);
-		tourBox.appendChild(buttonBox);
-		tourBox.classList.add("tourBox");
+    if (this.currentStep == this.steps.length - 1) {
+      buttons[1].classList.add("ft-hidden");
+      buttons[2].classList.add("ft-hidden");
+    } else {
+      buttons[3].classList.add("ft-hidden");
+    }
+
+    const placement = this.findPlacement(element, pos)
+		tourBox.classList.add("ft-tour-box");
     tourBox.style.cssText = placement;
+    element.style.position = "relative"; // the tour selector must be made relative so that the tip icon and tip box can be displayed properly.
 		element.appendChild(tourBox);
+    tourBox.scrollIntoView(false);
 	}
 
   findPlacement(el, pos) {
@@ -149,8 +171,7 @@ class Tour {
 		} else {
 			this.initializeStep(this.steps[this.currentStep]);
 		}
-		//go to the next step
-	}
+	} //go to the next step
 
 	back() {
 		this.deinitializeStep();
@@ -159,8 +180,7 @@ class Tour {
 			this.currentStep == 0;
 		}
 		this.initializeStep(this.steps[this.currentStep]);
-		// got to the previous step
-	}
+	} // got to the previous step
 
 	end() {
 		this.deinitializeStep();
